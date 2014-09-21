b305a091f55c14ba91acef615922b69ababa5844
Tarwine <tarwine@live.com>
2014-09-21 18:25:24 -0400

    Scrap class

---
+++ Source/Dieseland/Private/ScrapBox.cpp
@@ -0,0 +1,13 @@
+
+
+#include "Dieseland.h"
+#include "ScrapBox.h"
+
+
+AScrapBox::AScrapBox(const class FPostConstructInitializeProperties& PCIP)
+	: Super(PCIP)
+{
+
+}
+
+
---
+++ Source/Dieseland/Public/ScrapBox.h
@@ -0,0 +1,18 @@
+
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "ScrapBox.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DIESELAND_API AScrapBox : public AActor
+{
+	GENERATED_UCLASS_BODY()
+
+
+
+};
