86d3eaf5a993d6dcd08c94856d3f3d1a19a648af
Tarwine <tarwine@live.com>
2014-11-17 15:17:31 -0500

    Team color array

--- Source/Dieseland/Private/DieselandGameState.cpp
+++ Source/Dieseland/Private/DieselandGameState.cpp
@@ -18,6 +18,16 @@ ADieselandGameState::ADieselandGameState(const class FPostConstructInitializePro
 	WinningTeam = 0;
 	GameDuration = 60.0f;
 	GameTimer = GameDuration;
+
+	TeamColors.Add(FVector(0.000905f, 1.0f, 0.0f));
+	TeamColors.Add(FVector(0.035f, 0.005232f, 0.004898f));
+	TeamColors.Add(FVector(0.0f, 0.035871f, 1.0f));
+	TeamColors.Add(FVector(1.0f, 1.0f, 1.0f));
+	TeamColors.Add(FVector(0.0f, 0.828977f, 1.0f));
+	TeamColors.Add(FVector(1.0f, 0.935999f, 0.0f));
+	TeamColors.Add(FVector(0.747108f, 0.0f, 1.0f));
+	TeamColors.Add(FVector(1.0f, 0.305141f, 0.0f));
+	TeamColors.Add(FVector(1.0f, 0.131611f, 0.925403f));
 }
 
 void ADieselandGameState::ReceiveBeginPlay()
@@ -114,6 +124,7 @@ void ADieselandGameState::GetLifetimeReplicatedProps(TArray< FLifetimeProperty >
 	DOREPLIFETIME(ADieselandGameState, WinningScore);
 	DOREPLIFETIME(ADieselandGameState, WinningTeam);
 	DOREPLIFETIME(ADieselandGameState, TeamScores);
+	DOREPLIFETIME(ADieselandGameState, TeamColors);
 	DOREPLIFETIME(ADieselandGameState, GameDuration);
 	DOREPLIFETIME(ADieselandGameState, GameTimer);
 	DOREPLIFETIME(ADieselandGameState, Players);
